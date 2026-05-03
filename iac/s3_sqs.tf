df81dd7b0642e38398a95bacd54cec831dc92777
TiagoTZ0 <tayotrigoso@gmail.com>
2026-05-03 16:43:59 -0500

    feat: mejoras de seguridad, monitoreo y conectividad privada en Terraform

--- iac/s3_sqs.tf
+++ iac/s3_sqs.tf
@@ -1,13 +1,53 @@
 # S3 Bucket
 resource "aws_s3_bucket" "images" {
   bucket        = "image-processor-${terraform.workspace}-images-${var.bucket_suffix}"
-  force_destroy = true # Para que deje borrar todo con terraform destroy
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_versioning" "images" {
+  bucket = aws_s3_bucket.images.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "images" {
+  bucket = aws_s3_bucket.images.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "images" {
+  bucket = aws_s3_bucket.images.id
+  rule {
+    id     = "expire-uploads"
+    status = "Enabled"
+    filter {
+      prefix = "uploads/"
+    }
+    expiration {
+      days = 30
+    }
+  }
+  rule {
+    id     = "expire-processed"
+    status = "Enabled"
+    filter {
+      prefix = "processed/"
+    }
+    expiration {
+      days = 90
+    }
+  }
 }
 
 # SQS DLQ y Main
 resource "aws_sqs_queue" "dlq" {
   name                      = "image-processor-${terraform.workspace}-dlq"
-  message_retention_seconds = 1209600 # 14 dias
+  message_retention_seconds = 1209600
 }
 
 resource "aws_sqs_queue" "main" {
@@ -17,6 +57,36 @@ resource "aws_sqs_queue" "main" {
     deadLetterTargetArn = aws_sqs_queue.dlq.arn
     maxReceiveCount     = 3
   })
+}
+
+resource "aws_cloudwatch_log_group" "lambda_upload" {
+  name              = "/aws/lambda/upload-lambda-${terraform.workspace}"
+  retention_in_days = 14
+}
+
+resource "aws_cloudwatch_log_group" "lambda_crop" {
+  name              = "/aws/lambda/crop-lambda-${terraform.workspace}"
+  retention_in_days = 14
+}
+
+resource "aws_cloudwatch_log_group" "apigw" {
+  name              = "/aws/apigateway/image-api-${terraform.workspace}"
+  retention_in_days = 14
+}
+
+resource "aws_cloudwatch_metric_alarm" "dlq_alarm" {
+  alarm_name          = "dlq-messages-alarm-${terraform.workspace}"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "ApproximateNumberOfMessagesVisible"
+  namespace           = "AWS/SQS"
+  period              = 60
+  statistic           = "Sum"
+  threshold           = 0
+  alarm_description   = "Alarm if DLQ has any visible messages"
+  dimensions = {
+    QueueName = aws_sqs_queue.dlq.name
+  }
 }
 
 # Permiso para que S3 escriba en SQS
